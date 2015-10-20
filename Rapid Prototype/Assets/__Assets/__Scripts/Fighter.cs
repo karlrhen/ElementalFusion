ab024bcd575569e33f97e41a5df7fdaabe34f113
Karl R. Henderson <karlrhen@umich.edu>
2015-10-20 15:47:29 -0400

    Player can move successfully

--- Rapid Prototype/Assets/__Assets/__Scripts/Fighter.cs
+++ Rapid Prototype/Assets/__Assets/__Scripts/Fighter.cs
@@ -1,198 +1,114 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(CharacterController))]
 
 public class Fighter : MonoBehaviour {
-
-	/* Singleton */
+	/*
+	// Singleton 
 	public static Fighter S;
 
-	/* Fighter Stats */
-	public int HP;
-	public int curHP;
+	// Fighter Stats  
+	public int fighter_health;
 
-	/* Sprites */
-	public Sprite		  upSprite;
-	public Sprite		  downSprite;
-	public Sprite		  leftSprite;
-	public Sprite		  rightSprite;
+	// Sprites  
+	public Sprite		  up_sprite;
+	public Sprite		  left_sprite;
+	public Sprite		  right_sprite;
 	public SpriteRenderer sprend;
 
-	/* Movement */
-	public RaycastHit	hitInfo;
-	public bool 		moving = false;
-	public Vector3		targetPos;
-	public Direction 	direction;
-	public Vector3		moveVec;
-	public Vector3      start;
-	public float 		moveSpeed;
-	
-	public enum Direction{
-		down,
-		left,
-		up,
-		right
-	}
+	// Movement  
+	public Vector3		target_velocity;
+	public float 		movement_speed;
 	
 	new public Rigidbody rigidbody{
 		get {return gameObject.GetComponent<Rigidbody>();}
 	}
-	
-	public Vector3 pos{
-		get{return transform.position; }
-		set { transform.position = value;}
-	}
 
 	void Awake(){
 		S = this;
-		
 	}
 
 	void Start(){
-		
-		sprend = gameObject.GetComponent<SpriteRenderer> ();
-		
+		sprend = gameObject.GetComponent<SpriteRenderer> ();	
 	}
 	
 	void FixedUpdate(){
-		if (curHP <= 0)
-			gameObject.SetActive (false);
-		if (!moving) {
-			if(Input.GetKeyDown(KeyCode.A)){
-				//Action();
-				print ("Attack!");
-			}
-			if (Input.GetKey (KeyCode.RightArrow)) {
-				moveVec = Vector3.right;
-				direction = Direction.right;
-				sprend.sprite = rightSprite;
-				
-				moving = true;
-				
-			} else if (Input.GetKey (KeyCode.LeftArrow)) {
-				moveVec = Vector3.left;
-				direction = Direction.left;
-				sprend.sprite = leftSprite;
-				
-				moving = true;
-				
-			} else if (Input.GetKey (KeyCode.DownArrow)) {
-				moveVec = Vector3.down;
-				direction = Direction.down;
-				sprend.sprite = downSprite;
-				
-				moving = true;
-				
-			} else if (Input.GetKey (KeyCode.UpArrow)) {
-				moveVec = Vector3.up;
-				direction = Direction.up;
-				sprend.sprite = upSprite;
-				
-				moving = true;
-				
-				
-			} else {
-				moveVec = Vector3.zero;
-				moving = false;
-			}
-			
-			if(Physics.Raycast(GetRay(), out hitInfo, 1f, GetLayerMask(new string[] {"Ground"}))){
-				moveVec = Vector3.zero;
-				moving = false;
-				
-			}
-			
-			
-			targetPos = pos + moveVec;
-			
-		} else {
-			
-			if((targetPos - pos).magnitude < moveSpeed * Time.fixedDeltaTime ){
-				pos = targetPos;
-				moving = false;
-			}
-			else{
-				pos += (targetPos - pos).normalized * moveSpeed * Time.fixedDeltaTime;
-			}
-			
+		 target_velocity = rigidbody.velocity;
+
+		if(Input.GetKeyDown(KeyCode.A)){
+			print ("Attack!");
 		}
-		
-	}
-	
-	/*public void Action(){
-		Vector3 org = pos;
-		Direction orgd = direction;
-		direction = Direction.up;
-		bool hit = false;
-		for (int i = 0; i < curMove.rangeSize; i++) {
-			pos = curMove.range[i] + org;
-			if(Physics.Raycast(GetRay(), out hitInfo, 1f, GetLayerMask(new string[] {"Sign"}))){
-				hit = true;
-			}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			target_velocity.x += 1f;
+			sprend.sprite = right_sprite;
+		} 
+		else if (Input.GetKey (KeyCode.LeftArrow)) {
+			target_velocity.x -= 1f;
+			sprend.sprite = left_sprite;
+		} 
+		else if (Input.GetKey (KeyCode.UpArrow)) {
+			sprend.sprite = up_sprite;
+			target_velocity.y += 0.5f;
+		} 
+		else {
+			target_velocity.x = 0;
 		}
-		if (hit) {
-			Enemy.S.curHP -= curMove.damage;
-		}
-		else{
-			
-		}
-		pos = org;
-		
-		curMove.ShowSelf ();
-		
+		rigidbody.velocity = target_velocity;
 	}*/
-	
-	Ray GetRay(){
-		switch(direction){
-		case Direction.down:
-			return new Ray(pos,Vector3.down);
-		case Direction.left:
-			return new Ray(pos,Vector3.left);
-		case Direction.right:
-			return new Ray(pos,Vector3.right);
-		case Direction.up:
-			return new Ray(pos,Vector3.up);
-		default:
-			return new Ray();
+
+
+	/* Sprites */ 
+	public Sprite		  up_sprite;
+	public Sprite		  left_sprite;
+	public Sprite		  right_sprite;
+	public Sprite		  down_sprite;
+
+	public SpriteRenderer sprend;
+
+	/* Movement */
+	public float    speed         = 6.0F;
+	public float    jump_speed    = 8.0F;
+	public float    gravity       = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+
+	private CharacterController controller{
+		get{
+			return GetComponent<CharacterController>();
 		}
 	}
-	
-	int GetLayerMask(string[] layerNames){
-		int layerMask = 0;
-		
-		foreach(string layer in layerNames){
-			layerMask = layerMask | (1 << LayerMask.NameToLayer(layer));
-		}
-		
-		return layerMask;
+
+	void Update(){
+		Move ();
 	}
-	
-	/*public void AssignSprites(){
-		if (PokemonSelection.S.chosen_pokemon.Equals("Pikachu")) {
-			upSprite = pikachuUp;
-			downSprite = pikachuDown;
-			leftSprite = pikachuLeft;
-			rightSprite = pikachuRight;
-		} else if (PokemonSelection.S.chosen_pokemon.Equals("Bulbasaur")) {
-			upSprite    = bulbasaurUp;
-			downSprite  = bulbsaurDown;
-			leftSprite  = bulbasaurLeft;
-			rightSprite = bulbasaurRight;
+
+	void Move(){
+ 		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetKeyDown(KeyCode.Space)){
+				moveDirection.y = jump_speed;
+				sprend.sprite = up_sprite;
+			}
+			if(Input.GetKeyDown(KeyCode.DownArrow)){
+				print ("fix this");
+			}
+			if(Input.GetKeyDown(KeyCode.UpArrow)){
+				print ("fix this");
+			}
+			if(Input.GetKeyDown(KeyCode.LeftArrow)){
+				sprend.sprite = left_sprite;
+			}
+			if(Input.GetKeyDown(KeyCode.RightArrow)){
+				sprend.sprite = right_sprite;
+			}
 		}
-		else if (PokemonSelection.S.chosen_pokemon.Equals("Charmander")) {
-			upSprite    = charmanderUp;
-			downSprite  = charmanderDown;
-			leftSprite  = charmanderLeft;
-			rightSprite = charmanderRight;
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+
+		if (controller.velocity.x == 0 && controller.velocity.y == 0 && controller.isGrounded) {
+			sprend.sprite = down_sprite;
 		}
-		else if (PokemonSelection.S.chosen_pokemon.Equals("Squirtle")) {
-			upSprite    = squirtleUp;
-			downSprite  = squirtleDown;
-			leftSprite  = squirtleLeft;
-			rightSprite = squirtleRight;
-		}
-		
-	}*/
-	
-	
+	}
 }
